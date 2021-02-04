5160f27913cade318aec8c94346d3b0255c1b860
benedict-jackson <benedict.jackson@chameleontechsolutions.com>
2021-02-04 00:25:08 +0000

    enabled SSR styled components and created first keystone type

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,4 +1,13 @@
+import Router from 'next/router';
+import Nprogress from 'nprogress';
+// TODO: Swap with our own
+import '../components/styles/nprogress.css';
+
 import Page from '../components/Page';
+
+Router.events.on('routeChangeStart', () => Nprogress.start());
+Router.events.on('routeChangeComplete', () => Nprogress.done());
+Router.events.on('routeChangeError', () => Nprogress.done());
 
 export default function MyApp({ Component, pageProps }) {
   return (
